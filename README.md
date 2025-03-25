84f39041aa0a1ac36cc415e6b12b52a77ae46eb9
Joel jojo <86289226+imjoeljojo@users.noreply.github.com>
2025-03-25 12:30:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo
+ Todo list using webtech 
---
+++ todo.js
@@ -0,0 +1,48 @@
+var inputBox = document.getElementById("input-box");
+var addBtn = document.getElementById("addbtn");
+var taskListContainer = document.getElementById("task-list-cont");
+var events;
+var addTask = () => {
+  if (addBtn.innerHTML == "Edit") {
+    events.target.previousElementSibling.innerHTML = inputBox.value;
+    inputBox.value = "";
+    addBtn.innerHTML = "Add";
+  } else {
+    var inputBoxText = inputBox.value.trim();
+    if (!inputBoxText) {
+      alert("Enter the Task");
+      return false;
+    }
+    var taskList = document.createElement("li");
+    var taskContainer = document.createElement("p");
+    var editButton = document.createElement("button");
+
+    var deleteButton = document.createElement("button");
+
+    taskListContainer.appendChild(taskList);
+    taskList.appendChild(taskContainer);
+    taskList.appendChild(editButton);
+    taskList.appendChild(deleteButton);
+    // console.log("hello");
+    taskContainer.innerHTML = inputBoxText;
+    editButton.innerHTML = "Edit";
+    deleteButton.innerHTML = "Delete";
+
+    inputBox.value = "";
+  }
+};
+
+var update = (e) => {
+  if (e.target.innerHTML == "Edit") {
+    //when edit button is clicked
+    inputBox.value = e.target.previousElementSibling.innerHTML; //targets previous element(p tag) for editing
+    addBtn.innerHTML = "Edit";
+  } else if (e.target.innerHTML === "Delete") {
+    e.target.parentElement.remove(); //remove function is used to delete task
+    console.log(e.target.parentElement);
+  }
+  events = e;
+};
+
+addBtn.addEventListener("click", addTask);
+taskListContainer.addEventListener("click", update);
